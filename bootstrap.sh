eeda104d7331940ff6a9dc16691cbc2a99a56f96
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-05-02 15:18:00 -0300

    adding commands to run on bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,16 @@
-until nc -z -v -w30 naturallydb 3306
-do
+until nc -z -v -w30 todosdb 3306; do
   echo "Waiting for database connection..."
   sleep 10
 done
 
-if [ $APP_ENV = "local" ];
-then
-
-  CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-  if [ ! -e ./storage/$CONTAINER_ALREADY_STARTED ]; then
-    touch ./storage/$CONTAINER_ALREADY_STARTED
-    echo "** First container startup **"
-    composer install
-    php artisan migrate
-    echo "APP_KEY=" >> .env
-    php artisan key:generate
-    php artisan passport:install
-    chown 1000.1000 storage/*
-    chmod 775 /var/www/storage/logs
-    php artisan storage:link
-  fi
-fi
+composer install
+php artisan migrate:fresh
+php artisan config:cache
+php artisan key:generate
+php artisan passport:install
+chown 1000.1000 storage/*
+chmod 775 /var/www/storage/logs
+php artisan storage:link
 
 php artisan config:cache
 php artisan cache:clear
