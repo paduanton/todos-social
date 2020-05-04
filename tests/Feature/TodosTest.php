e404802a9812106c288c530e3b48edc6dfce2033
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-05-03 22:05:43 -0300

    todos unit test completed

--- tests/Feature/TodosTest.php
+++ tests/Feature/TodosTest.php
@@ -2,25 +2,17 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Http\Resources\TodosResource;
 use Tests\TestCase;
-use App\Http\Controllers\API\TodosController;
-use App\Todos;
-use Mockery;
-use Illuminate\Http\Request;
-
 use App\Users;
+use App\Todos;
+
 
 class TodosTest extends TestCase
 {
 
     public function testShouldCreateTodo()
     {
-        /*
-            It must have user with the id 1 in the database first
-        */
-
         $user = factory(Users::class)->create();
 
         $response = $this->actingAs($user, 'api')
@@ -36,11 +28,145 @@ class TodosTest extends TestCase
         $response
             ->assertStatus(201)
             ->assertJson([
-                'created' => true,
+                'title' => 'Lorem ipsum',
             ])->assertJsonFragment([
-
+                'title' => 'Lorem ipsum',
+                'description' => 'Lorem ipsum',
+                'completed' => true
             ])->assertJsonStructure([
-                
+                'id',
+                'users_id',
+                'title',
+                'description',
+                'completed',
+                'images',
+                'comments',
+                'created_at',
+                'updated_at',
             ]);
+    }
+
+    public function testShouldUpdateTodo()
+    {
+        /*
+            It must have a todo with the id 1 in the database first
+        */
+
+        $user = factory(Users::class)->create();
+
+        $response = $this->actingAs($user, 'api')
+            ->withHeaders([
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ])->json('PUT', '/v1/todos/1', [
+                'title' => 'Now, this is a story',
+                'description' => 'Lorem ipsum',
+                'completed' => false
+            ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'title' => 'Now, this is a story',
+            ])->assertJsonFragment([
+                'title' => 'Now, this is a story',
+                'description' => 'Lorem ipsum',
+                'completed' => 0
+            ])->assertJsonStructure([
+                'id',
+                'users_id',
+                'title',
+                'description',
+                'completed',
+                'images',
+                'comments',
+                'created_at',
+                'updated_at',
+            ]);
+    }
+
+    public function testShouldGetTodo()
+    {
+        /*
+            It must have a todo with the id 2 in the database first
+        */
+
+        $user = factory(Users::class)->create();
+
+        $response = $this->actingAs($user, 'api')
+            ->withHeaders([
+                'Accept' => 'application/json',
+            ])->json('GET', '/v1/todos/2');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'id',
+                'users_id',
+                'title',
+                'description',
+                'completed',
+                'images',
+                'comments',
+                'created_at',
+                'updated_at',
+            ]);
+    }
+
+    public function testShouldDeleteTodo()
+    {
+        /*
+            It must have a todo with the id 1 in the database first
+        */
+
+        $user = factory(Users::class)->create();
+
+        $response = $this->actingAs($user, 'api')
+            ->withHeaders([
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ])->json('DELETE', '/v1/todos/1');
+
+        $response->assertNoContent($status = 204);
+    }
+
+    public function testShouldGetAllTodos()
+    {
+        $user = factory(Users::class)->create();
+
+        $response = $this->actingAs($user, 'api')
+            ->withHeaders([
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ])->json('GET', '/v1/todos');
+
+        $response->assertStatus(200);
+    }
+
+    public function testShouldGetUnauthorized()
+    {
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ])->json('GET', '/v1/todos');
+
+        $response->assertUnauthorized();
+
+    }
+
+    public function testShouldNotGetTodo()
+    {
+        /*
+            It must not have a todo with the id 1211332 in the database first
+        */
+
+        $user = factory(Users::class)->create();
+
+        $response = $this->actingAs($user, 'api')
+            ->withHeaders([
+                'Accept' => 'application/json',
+            ])->json('GET', '/v1/todos/1211332');
+
+        $response->assertNotFound();
     }
 }
